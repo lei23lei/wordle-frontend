cddabe4e525e25561f22ca2b808ce50be7120df5
peter <lei23lei91@gmail.com>
2025-07-21 11:43:58 -0400

    prevent auto zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className="">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body className="bg-background relative  min-h-screen  ">
         <ThemeProvider
           attribute="class"
